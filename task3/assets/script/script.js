58411bd0541ecec1bcd70e4632b8ae89a58e7602
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2023-11-22 00:02:57 +0300

    add new code js

--- task3/assets/script/script.js
+++ task3/assets/script/script.js
@@ -2,33 +2,39 @@
 const image1 = "./assets/imgs/work_and_travel-forest.webp";
 const image2 = "./assets/imgs/work_and_travel-sea.webp";
 
-//элементы DOM дерева
-const image = document.getElementById('image');
-const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
+// //элементы DOM дерева
+// const image = document.getElementById('image');
+// const prevButton = document.getElementById('prevButton');
+// const nextButton = document.getElementById('nextButton');
 
-//текущая картинка
-let currentImage = image1;
+// //текущая картинка
+// let currentImage = image1;
 
-//изменение изображения вперед
-function changeImage() {
-	image.src = image2;
+// //изменение изображения вперед
+// function changeImage() {
+// 	image.src = image2;
+// }
+
+// //нажатие на кнопку вперед
+// nextButton.addEventListener('click', ()=>{
+// 	currentImage = image2;
+// 	changeImage();
+// });
+
+// //изменение изображения назад
+// function returnImage() {
+// 	image.src = image1;
+// }
+
+// //нажатие на кнопку назад
+// prevButton.addEventListener('click', ()=>{
+// 	currentImage = image1;
+// 	returnImage();
+// });
+
+function changeImage(path) {
+	image.src = path;
 }
 
-//нажатие на кнопку вперед
-nextButton.addEventListener('click', ()=>{
-	currentImage = image2;
-	changeImage();
-});
-
-//изменение изображения назад
-function returnImage() {
-	image.src = image1;
-}
-
-//нажатие на кнопку назад
-prevButton.addEventListener('click', ()=>{
-	currentImage = image1;
-	returnImage();
-});
-
+prevButton.addEventListener('click', ()=> changeImage(image1));
+nextButton.addEventListener('click', ()=> changeImage(image2));
